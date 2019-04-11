0fae476fae8627cb15a0e2f8e517fba0477b50c6
isaevyaroslav <iyaroslav@yandex.ru>
2019-04-11 20:27:00 +0300

    024 Added sounds and steps basic interface.

---
+++ Labyrinth-Unity/Assets/Scripts/Steps/StepsSoundPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LabyrinthUnity.StepsNS
+{
+    public class StepsSoundPlayer
+    {
+        private AudioSource _audioSource;
+        public StepsSoundPlayer(AudioSource audioSource)
+        {
+            _audioSource = audioSource;
+        }
+
+        public void PlayStepsSound(SoundsList soundsList)
+        {
+
+        }
+        private void _playStepSound(AudioSource audioSource, AudioClip audioClip)
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+}
